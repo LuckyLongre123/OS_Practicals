e6329b07adf7ca7afe388ad8983b9b2e73b987c4
Lucky Longre <officialluckylongre@gmail.com>
2025-11-09 09:53:55 +0530

    Practical 1 (b) added

--- Practical_01/b.py
+++ Practical_01/b.py
@@ -1,23 +1,19 @@
 import os
+print("main program started")
 
-def main():
-    print("main program started")
+pid = os.fork()
 
-    pid = os.fork()
-
-    if pid < 0:
-        print("fork failed!")
-    elif pid == 0:
-        print("this is child process")
-        print("child's PID: " + str(os.getpid()))
-        print("Child doing child work")
-        for i in range(3):
-            print(f"child work:  {i}")
-    else:
-        print("Parent's PID: " + str(os.getpid()))
-        print("parent doing parent work")
-        for i in range(3):
-            print(f"parent work:  {i}")
-        os.wait()
-
-main()
+if pid < 0:
+    print("fork failed!")
+elif pid == 0:
+    print("this is child process")
+    print("child's PID: " + str(os.getpid()))
+    print("Child doing child work")
+    for i in range(3):
+        print(f"child work:  {i}")
+else:
+    print("Parent's PID: " + str(os.getpid()))
+    print("parent doing parent work")
+    for i in range(3):
+        print(f"parent work:  {i}")
+    os.wait()
