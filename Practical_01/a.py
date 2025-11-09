c067733f96adfa1329d94ab54c90d51e3d8f0c07
Lucky Longre <officialluckylongre@gmail.com>
2025-11-09 09:52:28 +0530

    Practical 1 (a) added

--- Practical_01/a.py
+++ Practical_01/a.py
@@ -1,20 +1,15 @@
 import os
+print("main program started")
 
-def main():
-    print("main program started")
+pid = os.fork()
 
-    pid = os.fork()
+if pid < 0:
+    print("fork failed!")
+    return
 
-    if pid < 0:
-        print("fork failed!")
-        return
-
-    if pid == 0:
-        print("this is child process")
-        print("child's PID: " + str(os.getpid()))
-    else:
-        print("this is parent process")
-        print("parent's PID: " + str(os.getpid()))
-        
-if __name__ == "__main__":
-    main()
+if pid == 0:
+    print("this is child process")
+    print("child's PID: " + str(os.getpid()))
+else:
+    print("this is parent process")
+    print("parent's PID: " + str(os.getpid()))
