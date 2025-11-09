cdffce70f95b1804e09d8401e4f9dd810d9cec82
Lucky Longre <officialluckylongre@gmail.com>
2025-11-09 10:03:11 +0530

    Practical 2 added

--- Practical_02/main.py
+++ Practical_02/main.py
@@ -1,3 +1,14 @@
-def main():
-    try:
-        with open('/proc/version','r')
+import platform
+try:
+    with open('/proc/version','r') as f:
+        print("kernal version :"+f.read())
+    print("CPU informations")
+    with open('/proc/cpuinfo', 'r') as f:
+        for line in f:
+                if 'model name' in line or 'cpu cores' in line or 'processor' in line:
+                    print(line.strip())
+except FileNotFoundError:
+        print("Running on non-Linux system. Using alternative method:")
+        print("System: " + platform.system())
+        print("Release: " + platform.release())
+        print("Processor: " + platform.processor())
