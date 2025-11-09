0e7a29c5ff50c962d192ea7cbe9f4198bf3bb28c
Lucky Longre <officialluckylongre@gmail.com>
2025-11-09 09:55:24 +0530

    Practical 1 (c) added

--- Practical_01/c.py
+++ Practical_01/c.py
@@ -1,22 +1,19 @@
 import os
 import time
 
-def main():
-    print("main program started")
+print("main program started")
 
-    pid = os.fork()
+pid = os.fork()
 
-    if pid < 0:
-        print("fork failed!")
-    elif pid == 0:
-        print("this is child process")
-        print("child's PID: " + str(os.getpid()))
-        time.sleep(2)
-        print("child work done")
-    else:
-        print("Parent's PID: " + str(os.getpid()))
-        child_pid,status = os.wait()
-        print(f"parent process Child: {child_pid} finished with status {status}")
-        print("parent process terminated")
-
-main()
+if pid < 0:
+    print("fork failed!")
+elif pid == 0:
+    print("this is child process")
+    print("child's PID: " + str(os.getpid()))
+    time.sleep(2)
+    print("child work done")
+else:
+    print("Parent's PID: " + str(os.getpid()))
+    child_pid,status = os.wait()
+    print(f"parent process Child: {child_pid} finished with status {status}")
+    print("parent process terminated")
