63047465986c5757cfe7a0dee16ff6572a6a86cc
Lucky Longre <officialluckylongre@gmail.com>
2025-11-13 04:59:23 +0000

    fileas added

--- Practical_08/main.py
+++ Practical_08/main.py
@@ -1,42 +1,42 @@
-num_processes = int(input("Enter number of processes: "))
-burst_times = []
-index = 0
-while index < num_processes:
-    burst_times.append(int(input("Enter burst time for process " + str(index+1) + ": ")))
-    index += 1
-
-process_ids = list(range(1, num_processes + 1))
+n = int(input("Enter number of processes: "))
+b_t = []
 i = 0
-while i < num_processes:
+while i < n:
+    b_t.append(int(input("Enter burst time for process " + str(i+1) + ": ")))
+    i += 1
+
+pids = list(range(1, n + 1))
+i = 0
+while i < n:
     j = i + 1
-    while j < num_processes:
-        if burst_times[i] > burst_times[j]:
-            burst_times[i], burst_times[j] = burst_times[j], burst_times[i]
-            process_ids[i], process_ids[j] = process_ids[j], process_ids[i]
+    while j < n:
+        if b_t[i] > b_t[j]:
+            b_t[i], b_t[j] = b_t[j], b_t[i]
+            pids[i], pids[j] = pids[j], pids[i]
         j += 1
     i += 1
 
-waiting_times = [0] * num_processes
-turnaround_times = [0] * num_processes
+w_t = [0] * n
+t_t = [0] * n
 
 i = 1
-while i < num_processes:
-    waiting_times[i] = waiting_times[i - 1] + burst_times[i - 1]
+while i < n:
+    w_t[i] = w_t[i - 1] + b_t[i - 1]
     i += 1
 
 i = 0
-while i < num_processes:
-    turnaround_times[i] = waiting_times[i] + burst_times[i]
+while i < n:
+    t_t[i] = w_t[i] + b_t[i]
     i += 1
 
 print("\nSJF (Shortest Job First) CPU Scheduling")
 print("Process\tBurst Time\tWaiting Time\tTurnaround Time")
 i = 0
-while i < num_processes:
-    print(str(process_ids[i]) + "\t" + str(burst_times[i]) + "\t\t" + str(waiting_times[i]) + "\t\t" + str(turnaround_times[i]))
+while i < n:
+    print(str(pids[i]) + "\t" + str(b_t[i]) + "\t\t" + str(w_t[i]) + "\t\t" + str(t_t[i]))
     i += 1
 
-avg_wt = sum(waiting_times) / num_processes
-avg_tat = sum(turnaround_times) / num_processes
+avg_wt = sum(w_t) / n
+avg_tat = sum(t_t) / n
 print("\nAverage Waiting Time: " + str(avg_wt))
 print("Average Turnaround Time: " + str(avg_tat))
